537acdfad8356930b2e08b7e50552026701cb3a7
Lucca Sander <lsanderf08@gmail.com>
2026-09-18 10:14:20 -0300

    chore: configura ambiente Docker e PostgreSQL

--- src/main/java/com/Lucca/Projeto1/config/SecurityConfig.java
+++ src/main/java/com/Lucca/Projeto1/config/SecurityConfig.java
@@ -27,7 +27,12 @@ import org.springframework.security.oauth2.server.resource.web.authentication.Be
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
 
 import java.util.Arrays;
 import java.util.List;
@@ -39,6 +44,35 @@ public class SecurityConfig {
     private final Environment environment;
     private final ApiSecurityErrorWriter errorWriter;
     private final UsuarioRepository usuarioRepository;
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173"
+        ));
+
+        configuration.setAllowedMethods(List.of(
+                "GET",
+                "POST",
+                "PUT",
+                "PATCH",
+                "DELETE",
+                "OPTIONS"
+        ));
+
+        configuration.setAllowedHeaders(List.of("*"));
+
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
 
     public SecurityConfig(
             Environment environment,
